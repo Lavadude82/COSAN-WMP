2c71a5c5752e91fff2c3de1e876533065739d987
Lavadude82 <irvinduarte2009@gmail.com>
2023-08-02 18:35:48 -0500

    starting js files

--- main/service.js
+++ main/service.js
@@ -1,1 +1,24 @@
-const 
+const fs = require("fs");
+const config = require("../conf/config.json");
+config.paths.forEach((e, i) => {
+  let files;
+  if (e.startsWith("/")) {
+    try {
+      files = fs.readdirSync(e);
+    } catch (err) {
+      console.log(
+        `error : specified path "${e}" not found\nThis will NOT STOP the program!`
+      );
+    }
+  } else {
+    try {
+      files = fs.readdirSync(__dirname + "/../" + e);
+    } catch (err) {
+      console.log(
+        `error : specified path "${
+          __dirname.substring(0, __dirname.lastIndexOf("/")) + "/" + e
+        }" not found\nThis will NOT STOP the program!`
+      );
+    }
+  }
+});
