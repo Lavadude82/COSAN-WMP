caef47949747087f81905a9c04f702641091aea0
Lavadude82 <irvinduarte2009@gmail.com>
2023-08-02 19:32:10 -0500

    added GetMedia() method

--- main/service.js
+++ main/service.js
@@ -1,24 +1,55 @@
+//Setting up variables
 const fs = require("fs");
-const config = require("../conf/config.json");
-config.paths.forEach((e, i) => {
-  let files;
-  if (e.startsWith("/")) {
-    try {
-      files = fs.readdirSync(e);
-    } catch (err) {
-      console.log(
-        `error : specified path "${e}" not found\nThis will NOT STOP the program!`
-      );
+const conf = require("../conf/config.json");
+const express = require("express");
+const app = express();
+const electron = require("electron");
+
+//Starting Application
+console.log("Starting App...");
+function GetMedia(config) {
+  let filesInPath = [];
+  config.paths.forEach((e, i) => {
+    let files;
+    if (e.startsWith("/")) {
+      try {
+        files = fs.readdirSync(e);
+      } catch (err) {
+        console.log(`error : specified path "${e}" not found!`);
+      }
+    } else {
+      try {
+        files = fs.readdirSync(__dirname + "/../" + e);
+      } catch (err) {
+        console.log(
+          `error : specified path "${
+            __dirname.substring(0, __dirname.lastIndexOf("/")) + "/" + e
+          }" not found!`
+        );
+      }
     }
-  } else {
-    try {
-      files = fs.readdirSync(__dirname + "/../" + e);
-    } catch (err) {
-      console.log(
-        `error : specified path "${
-          __dirname.substring(0, __dirname.lastIndexOf("/")) + "/" + e
-        }" not found\nThis will NOT STOP the program!`
-      );
-    }
-  }
+    filesInPath.push(files);
+  });
+  filesInPath.forEach((e1, i1) => {
+    e1.forEach((e2, i2) => {
+      let isFormat = false;
+      config.formats.forEach((e3, i3) => {
+        if (e2.endsWith(e3)) {
+          isFormat = true;
+        }
+      });
+      if (!isFormat) {
+        filesInPath[i1].splice(i2, 1);
+      }
+    });
+  });
+  let tmp = filesInPath;
+  tmp.forEach((e, i) => {
+    let f = e;
+    filesInPath[i] = { files: f, dir: config.paths[i] };
+  });
+  return filesInPath;
+}
+app.post("/getMedia", (req, res) => {
+  let files = GetMedia(conf);
 });
